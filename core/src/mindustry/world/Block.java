fc2376525768d83e8d914aa17a58a20b1d762987
buthed010203 <naguiar010203@gmail.com>
2026-03-25 02:23:19 -0400

    Fix health bars not having fixed characters which causes them to be jittery

--- core/src/mindustry/world/Block.java
+++ core/src/mindustry/world/Block.java
@@ -678,8 +678,8 @@ public class Block extends UnlockableContent implements Senseable{
     }
 
     public void setBars(){
-        addBar("health", entity -> new Bar(() -> 
-            Strings.format("@ (@ / @)", Core.bundle.get("stat.health"), Mathf.round(entity.health, 0.1f), Mathf.round(health, 0.1f))
+        addBar("health", entity -> new Bar(() ->
+            Strings.format("@ (@ / @)", Core.bundle.get("stat.health"), Strings.fixed(entity.health, 1),Strings.fixed(health, 1))
         , () -> Pal.health, entity::healthf).blink(Color.white));
 
         if(consPower != null){
